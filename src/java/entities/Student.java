8e8e982315221349ba9c307a98a1d73540047905
Irati <iratigarzon15@gmail.com>
2023-12-14 08:27:12 +0100

    Modifications

--- src/java/entities/Student.java
+++ src/java/entities/Student.java
@@ -6,16 +6,56 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Set;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author 2dam
+ * @author Irati
+ * @author Olivia
  */
 public class Student extends User implements Serializable{
+    @Enumerated(EnumType.STRING)
     private LevelType levelType;
+    @OneToMany(mappedBy = "student", fetch = FetchType.EAGER)
     private Set<Mark> marks;
-    
-    //Setters and Getters
+    @OneToMany(mappedBy = "student", fetch = FetchType.EAGER)
+    private Set<Enrolled> enrollments;
     //Constructors
+    public Student(String dni, String email, String name, String surname, String password, Date dateInit, UserType userType) {
+        super(dni, email, name, surname, password, dateInit, userType);
+    }
+
+    
+    public Student() {    
+    }
+    //Setters and Getters
+    public LevelType getLevelType() {
+        return levelType;
+    }
+
+    public void setLevelType(LevelType levelType) {
+        this.levelType = levelType;
+    }
+    @XmlTransient
+    public Set<Mark> getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Set<Mark> marks) {
+        this.marks = marks;
+    }
+    @XmlTransient
+    public Set<Enrolled> getEnrollments() {
+        return enrollments;
+    }
+
+    public void setEnrollments(Set<Enrolled> enrollments) {
+        this.enrollments = enrollments;
+    }
 }
--- src/java/entities/Teacher.java
+++ src/java/entities/Teacher.java
@@ -1,20 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
  * @author 2dam
  */
 class Teacher extends User implements Serializable{
+    @Enumerated(EnumType.STRING)
     private StudiesType studiesType;
     private String qualifications;
+    @OneToMany(mappedBy = "teacher")
+    private Set<Subject> subjects;
     
-    //Setters and Getters
     //Constructors
+
+    public Teacher(String dni, String email, String name, String surname, String password, Date dateInit, UserType userType) {
+        super(dni, email, name, surname, password, dateInit, userType);
+    }
+
+    public Teacher() {
+    }
+    //Setters and Getters
+
+    public StudiesType getStudiesType() {
+        return studiesType;
+    }
+
+    public void setStudiesType(StudiesType studiesType) {
+        this.studiesType = studiesType;
+    }
+
+    public String getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(String qualifications) {
+        this.qualifications = qualifications;
+    }
+    @XmlTransient
+    public Set<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Set<Subject> subjects) {
+        this.subjects = subjects;
+    }
+    
 }
